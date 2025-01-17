50213a1debcc82af4b605356446e7b3194675578
kohei3110 <kk.31108424@gmail.com>
2025-01-17 14:29:02 +0900

    files 永続化APIを作成

--- app/backend/startup.py
+++ app/backend/startup.py
@@ -7,6 +7,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 app = FastAPI()
 
 DATA_DIR = Path(os.getenv("DATA_DIR", "/data"))
+FILE_DIR = Path(os.getenv("FILE_DIR", "/files"))
 
 def delete_files():
     """Delete all files in the data directory."""
@@ -19,12 +20,22 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(delete_files, 'interval', seconds=30)
 scheduler.start()
 
-@app.post("/files/")
-async def upload_file(file: UploadFile = File(...)):
+@app.post("/data")
+async def upload_data(file: UploadFile = File(...)):
     """Upload a file to the data directory."""
     if not DATA_DIR.exists():
         DATA_DIR.mkdir(parents=True, exist_ok=True)
     save_path = DATA_DIR / file.filename
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename}
+
+@app.post("/files")
+async def upload_files(file: UploadFile = File(...)):
+    """Upload a file to the data directory."""
+    if not FILE_DIR.exists():
+        FILE_DIR.mkdir(parents=True, exist_ok=True)
+    save_path = FILE_DIR / file.filename
     with open(save_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     return {"filename": file.filename}
