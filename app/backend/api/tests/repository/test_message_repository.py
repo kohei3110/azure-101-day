bb8784d087af44c5e8f69f71cb5592347a24a0ba
kohei3110 <kk.31108424@gmail.com>
2025-02-03 16:26:50 +0900

    message_repositoryのテスト追加

--- app/backend/api/tests/repository/test_message_repository.py
+++ app/backend/api/tests/repository/test_message_repository.py
@@ -1,3 +1,11 @@
+from io import BytesIO
+from pathlib import Path
+import sys
+import os
+from unittest.mock import patch
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
+
 import pytest
 from pathlib import Path
 import json
