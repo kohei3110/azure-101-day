1d9731997745511f73ebe5dbd392374bc48ede49
kohei3110 <kk.31108424@gmail.com>
2025-02-03 16:14:54 +0900

    モジュールのディレクトリ修正

--- app/backend/api/tests/services/test_file_upload_service.py
+++ app/backend/api/tests/services/test_file_upload_service.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
 
 import shutil
 from pathlib import Path
